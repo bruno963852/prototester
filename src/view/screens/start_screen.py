0988c5acdd9345ed5b646b5c760f5169a78c6a8e
Bruno Martins <bruno963852@gmail.com>
2021-06-23 21:11:51 -0300

    refactored ui

--- src/view/screens/start_screen.py
+++ src/view/screens/start_screen.py
@@ -10,11 +10,6 @@ class StartScreen:
     @staticmethod
     def show():
         oled = OLED.instance()
-        oled.fill(0)
-        oled.rect(0, 0, 128, 64, 255)
-        oled.fill_rect(0, 0, 18, 8 + SPACING, 0xff)
-        oled.fill_rect(110, 0, 18, 8 + SPACING, 0xff)
-        oled.text(TITLE, int((128 - (len(TITLE) * 8)) / 2), 2)
-        draw_icon(oled, prototester, int((128 - 22) / 2) + 1, 15)
-        oled.text(SUBTITLE, int((128 - (len(SUBTITLE) * 8)) / 2), 54)
+        oled.clear()
+        oled.draw_window(TITLE)
         oled.show()
--- src/view/ui.py
+++ src/view/ui.py
@@ -1,19 +1,47 @@
 from machine import Pin, I2C
+from micropython import const
 from ssd1306 import SSD1306_I2C # pylint: disable=import-error
+
+SCL = const(22)
+SDA = const(21)
+
+SCREEN_WIDTH = const(128)
+SCREEN_HEIGHT = const(64)
+
+CHARACTER_SIZE = const(8)
+
+COLOR_BLACK = const(0x00)
+COLOR_WHITE = const(0xff)
 
 class OLED:
     _instance = None
 
     def __init__(self):
-        self._i2c = I2C(0, scl=Pin(22), sda=Pin(21))
-        self._oled = SSD1306_I2C(128, 64, self._i2c)
+        self._i2c = I2C(0, scl=Pin(SCL), sda=Pin(SDA))
+        self._oled = SSD1306_I2C(SCREEN_WIDTH, SCREEN_WIDTH, self._i2c)
 
     @classmethod
-    def instance(cls) -> SSD1306_I2C: # pylint: disable=undefined-variable
+    def instance(cls) -> OLED: # pylint: disable=undefined-variable
         if cls._instance is None:
             cls._instance = cls()
-        return cls._instance.oled
+        return cls._instance
 
     @property
     def oled(self) -> SSD1306_I2C:
         return self._oled
+
+    def clear(self):
+        self._oled.fill(COLOR_BLACK)
+
+    def show(self):
+        self._oled.show()
+
+    def draw_window(self, title: str):
+        self._oled.rect(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, COLOR_WHITE)
+        self._oled.fill_rect(0, 0, SCREEN_WIDTH, CHARACTER_SIZE + 2, COLOR_WHITE)
+        # self.draw_text_in_center(title, 2)
+
+    def draw_text_in_center(self, text: str, y_position):
+        text_width_pixels = len(text) * 8
+        left_padding = int((SCREEN_WIDTH - text_width_pixels) / 2)
+        self._oled.text(text, left_padding, y_position)
