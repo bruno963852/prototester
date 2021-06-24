e901b0f69afb9ebb794793dff73a00a705387e10
Bruno Martins <bruno963852@gmail.com>
2021-06-24 16:20:47 -0300

    Refactored ui elements, fixed start screen

--- src/view/screens/select_screen.py
+++ src/view/screens/select_screen.py
@@ -11,13 +11,9 @@ class SelectScreen:
         self.pages = pages_quant
 
     def show(self, options: list, selected_in_page: int, page_index: int):
-        oled = OLED.instance().oled
-        oled.fill(0)
-        oled.rect(0, 0, 128, 64, 255)
-        oled.text(TITLE, int((128 - (len(TITLE) * 8)) / 2), SPACING)
-        oled.fill_rect(0, 0, 38, 8 + SPACING, 0xff)
-        oled.fill_rect(len(TITLE) * 8 + 42, 0, 38, 8 + SPACING, 0xff)
-        oled.text('{}/{}'.format(page_index + 1, self.pages), 103, SPACING, 0x00)
+        oled = OLED.instance()
+        oled.clear()
+        oled.draw_window(TITLE, right_text='{}/{}'.format(page_index + 1, self.pages))
         for index, option in enumerate(options):
             y_position = ((index + 1) * SPACING) + ((index + 1)* CHAR_HEIGHT) + SPACING
             if index == selected_in_page:
@@ -25,4 +21,17 @@ class SelectScreen:
             else:
                 oled.fill_rect(SPACING, y_position, 8, 8, 0)
             oled.text(option.replace('.json', '')[:14], 10, y_position)
+        # oled.fill(0)
+        # oled.rect(0, 0, 128, 64, 255)
+        # oled.text(TITLE, int((128 - (len(TITLE) * 8)) / 2), SPACING)
+        # oled.fill_rect(0, 0, 38, 8 + SPACING, 0xff)
+        # oled.fill_rect(len(TITLE) * 8 + 42, 0, 38, 8 + SPACING, 0xff)
+        # oled.text('{}/{}'.format(page_index + 1, self.pages), 103, SPACING, 0x00)
+        # for index, option in enumerate(options):
+        #     y_position = ((index + 1) * SPACING) + ((index + 1)* CHAR_HEIGHT) + SPACING
+        #     if index == selected_in_page:
+        #         oled.text('>', SPACING, y_position)
+        #     else:
+        #         oled.fill_rect(SPACING, y_position, 8, 8, 0)
+        #     oled.text(option.replace('.json', '')[:14], 10, y_position)
         oled.show()
